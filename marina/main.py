e670d7ba0ed26e8ebc8baf6ef61b548e3abf06eb
Haikouhi <h.oroudjian@protonmail.com>
2019-07-05 15:52:55 +0200

    code marina

--- marina/main.py
+++ marina/main.py
@@ -5,6 +5,6 @@ im = Image.open('bon_commande.jpeg')
 text = pytesseract.image_to_string(im, lang="french")
 #text = pytesseract.image_to_string(Image.open('bon_commande.jpeg'))
 print(text)"""
-text = pytesseract.image_to_string(Image.open("fantastique.jpg"))
+text = pytesseract.image_to_string(Image.open("bon-de-commande.jpg"))
 print(text)
-print(pytesseract.image_to_string(Image.open("fantastique.jpg")))
+#print(pytesseract.image_to_string(Image.open("fantastique.jpg")))
