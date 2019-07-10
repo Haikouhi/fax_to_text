4398ec4d8ffe975da2f498715094ebadbdb51a5d
Haikouhi <h.oroudjian@protonmail.com>
2019-07-10 14:43:25 +0200

    testing

--- haikouhi/main.py
+++ haikouhi/main.py
@@ -1,45 +1,33 @@
 import requests
-# If you are using a Jupyter notebook, uncomment the following line.
-# %matplotlib inline
-import matplotlib.pyplot as plt
-from PIL import Image
-from io import BytesIO
+import sys
 
-# Replace <Subscription Key> with your valid subscription key.
-subscription_key = "f0dd69faa4384e0ca63c37ffbccb2245"
-assert subscription_key
-
-# You must use the same region in your REST call as you used to get your
-# subscription keys. For example, if you got your subscription keys from
-# westus, replace "westcentralus" in the URI below with "westus".
-#
-# Free trial subscription keys are generated in the "westcentralus" region.
-# If you use a free trial subscription key, you shouldn't need to change
-# this region.
+subscription_key = ""
 vision_base_url = "https://francecentral.api.cognitive.microsoft.com/vision/v2.0/"
+ocr_url = vision_base_url + "ocr"
 
-analyze_url = vision_base_url + "analyze"
+def image_to_text(image_file, output_file):
+    image_data = open('/home/haikouhi/simplon/fax_to_text/images/1.jpg', "rb").read()
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key,
+               'Content-Type': 'application/octet-stream'}
+    response = requests.post(ocr_url, headers=headers, data=image_data)
+    response.raise_for_status()
+    analysis = response.json()
 
-# Set image_path to the local path of an image that you want to analyze.
-image_path = "/home/haikouhi/simplon/fax_to_text/images/1.jpg"
+    regions = analysis["regions"]
+    lines = [region["lines"] for region in regions][0]
+    words = [line["words"] for line in lines]
+    lines_words = []
+    for line_words in words:
+        w = [lw["text"] for lw in line_words]
+        lines_words.append(w)
 
-# Read the image into a byte array
-image_data = open(image_path, "rb").read()
-headers = {'Ocp-Apim-Subscription-Key': subscription_key,
-           'Content-Type': 'application/octet-stream'}
-params = {'visualFeatures': 'Categories,Description,Color'}
-response = requests.post(
-    analyze_url, headers=headers, params=params, data=image_data)
-response.raise_for_status()
+    with open(output_file, "w+") as output:
+        for lw in lines_words:
+            output.write(' '.join(lw))
+            output.write('\n')
 
-# The 'analysis' object contains various fields that describe the image. The most
-# relevant caption for the image is obtained from the 'description' property.
-analysis = response.json()
-print(analysis)
-image_caption = analysis["description"]["captions"][0]["text"].capitalize()
+def ocr_text(image_file):
+    image_to_text(image_file, image_file.replace(".jpg", ".txt"))
 
-# Display the image and overlay it with the caption.
-image = Image.open(BytesIO(image_data))
-plt.imshow(image)
-plt.axis("off")
-_ = plt.title(image_caption, size="x-large", y=-0.1)
+if __name__ == "__main__":
+    ocr_text(sys.argv[1])
