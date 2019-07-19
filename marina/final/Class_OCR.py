fa8b77613da4957633424a64dda7c812ce38d9c4
marinacha <48958470+marinacha@users.noreply.github.com>
2019-07-19 14:29:35 +0200

    Update Class_OCR.py

--- marina/final/Class_OCR.py
+++ marina/final/Class_OCR.py
@@ -12,7 +12,11 @@ class Ocr:
         self.params = {'language': 'unk', 'detectOrientation': 'true'}
         self.data = {'url': self.image_url}
         self.response = requests.post(ocr_url, headers=self.headers, params=self.params, json=self.data)
+        print("self.response = ", self.response)
         self.response.raise_for_status()
+        self.analysis = None
+        self.text = None
+        self.dico = None
 
     #### OCR ####
     def ocr_system(self):
@@ -29,22 +33,23 @@ class Ocr:
                     word_infos.append(word_info)
                     text = text + " " + word_info["text"]
 
+        self.analysis = analysis
+        self.text = text
         # OCR analysis file
-        with open("text_analysis.json", "w") as f_write:
+        with open("text_analysis_HEROKU.json", "w") as f_write:
             json.dump(analysis, f_write, indent=4, ensure_ascii=False)
 
         # extracted text file
-        with open("text_brut.txt", "w") as f:
+        with open("text_brut_HEROKU.txt", "w") as f:
             json.dump(text, f, indent=4, ensure_ascii=False)
 
         # output extracted keywords
-        with open("dico_output.json", "w") as file:
+        with open("dico_output_HEROKU.json", "w") as file:
             dico = {'no_commande': [],
                     'no_client': [],
                     'mails' : [],
                     'tel': [],
-                    'no_ref': ""
-                   }
+                    'no_ref': ""}
 
             # regex for output extraction
             mails = re.search(r"([a-zA-Z][a-zA-Z0-9-._]*@[a-zA-Z]+.[a-zA-Z]{1,4})", str(text))
@@ -63,21 +68,6 @@ class Ocr:
             if no_ref:
                 dico['no_ref'] = no_ref
 
+            self.dico = dico
+            print("DICO = ", dico)
             json.dump(dico, file, indent=4, ensure_ascii=False)
-
-        """
-        # Display the image and overlay it with the extracted text.
-        plt.figure(figsize=(5, 5))
-        image = Image.open(BytesIO(requests.get(self.image_url).content))
-        ax = plt.imshow(image, alpha=0.5)
-        for word in word_infos:
-            bbox = [int(num) for num in word["boundingBox"].split(",")]
-            text = word["text"]
-            origin = (bbox[0], bbox[1])
-            patch = Rectangle(origin, bbox[2], bbox[3],
-                              fill=False, linewidth=2, color='y')
-            ax.axes.add_patch(patch)
-            plt.text(origin[0], origin[1], text, fontsize=20, weight="bold", va="top")
-            print(text)
-        plt.axis("off")
-        """
